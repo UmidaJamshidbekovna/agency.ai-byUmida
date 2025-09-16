26e825d91e3bd34ba17bf3b13091371c656f4be5
UmidaJamshidbekovna <umidarakhimova34@gmail.com>
2025-09-17 00:47:01 +0500

    keep going on...

---
+++ src/components/Services.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import assets from '../assets/assets'
+import Title from './Title'
+
+const Services = () => {
+
+    const servicesData = [
+        {
+            title: "Advertising",
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, non.',
+            icon: assets.ads_icon
+        },
+         {
+            title: "Content marketing",
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, non.',
+            icon: assets.marketing_icon
+        },
+        {
+            title: "Content writing",
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, non.',
+            icon: assets.content_icon
+        },
+         {
+            title: "Social Media",
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, non.',
+            icon: assets.social_icon
+        },
+    ]
+
+  return (
+    <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+      
+        <img src={assets.bgImage2} alt=""  className=' absolute -top-110 -left-70 -z-1 dark:hidden'/>
+
+        <Title title={"How can we help ?"} desc={"From strategy to execution, we craft digital solutions that move your business forward"}/>
+
+    </div>
+  )
+}
+
+export default Services
