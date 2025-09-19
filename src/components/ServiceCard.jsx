38aa101c7c658c310aefcb7957d0448b9702a7de
UmidaJamshidbekovna <umidarakhimova34@gmail.com>
2025-09-20 01:21:49 +0500

    keep going on...

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -4,7 +4,7 @@ const ServiceCard = () => {
   return (
     <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10'>
 
-        <div className=''>
+        <div className='pointer-events-none blur-2xl  rounded-full bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 w-[300px] h-[300px] absolute z-0  transition-opacity  duration-500  mix-blend-lighten'>
 
         </div>
 
